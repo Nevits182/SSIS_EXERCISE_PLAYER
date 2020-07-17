44bf23d5fabbe2aede8c0550d1d1523dd1a1759c
Nevits182 <nevits182>
2020-07-17 12:40:32 -0500

    subiendo solución ETL

---
+++ Bat/exec_SucessFile_D.bat
@@ -0,0 +1,8 @@
+set anio=%date:~6,4%
+set mes=%date:~3,2%
+set dia=%date:~0,2%
+set hora=%time:~0,2%
+set hora=%hora: =0%
+set minuto=%time:~3,2%
+
+Copy "C:\Test\files to load\Test file D.csv" C:\Test\Backup-Folder\Test_file_D\TestD_%anio%%mes%%dia%_%hora%%minuto%.csv /Y
